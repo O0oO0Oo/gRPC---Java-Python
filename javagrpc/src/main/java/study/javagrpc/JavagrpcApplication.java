b06510c623434cea6da247a85c2a1f326fd4eb98
BA <windy01999@gmail.com>
2023-10-19 13:54:55 +0900

    feat: 초기 프로젝트 세팅

---
+++ javagrpc/src/main/java/study/javagrpc/JavagrpcApplication.java
@@ -0,0 +1,13 @@
+package study.javagrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavagrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavagrpcApplication.class, args);
+	}
+
+}
